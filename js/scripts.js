ef5e6160a93b900179f7339e24e8a6e3361758e7
Student <example@example.com>
2019-11-08 12:55:00 -0800

    Add User Interface Logic.

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,41 @@
-$(document).ready(functio(){
+// Business Logic
 
+function Pizza(size,toppings){
+  this.size = size;
+  this.toppings = toppings;
+  this.price = 0;
+}
 
-});
+Pizza.prototype.getPrice = function(){
+  this.toppings.forEach(functio(topping))
+    this.topping += 1;
+  }
+  if(this.size === "small") {
+    this.price += 10;
+    console.log();
+  } else if (this.size === "medium") {
+    this.price += 4;
+  }else if (this.size === "large") {
+    this.price += 8;
+  }
+
+// return this.price;
+
+// User Interface Logic
+
+$(document).ready(function(event) {
+  $("form#pizzaOrder").submit(function(event){
+    event.preventDefault();
+    var toppingsInput = $("#toppings:checked");
+    var toppingsArray = [];
+    var sizeInput = $("#size").val();
+
+    toppingsInputs.forEach(function(toppingInput) {
+      toppingsArray.push(toppingInput.val());
+    });
+
+    var Pizza = new Pizza(SizeInput,ToppingsArray);
+    var price = Pizza.getPrice();
+
+  })
+})
